3feee15375f79f114cf45894553df088c1e4c73c
Iqoqacola <iqoqacola@gmail.com>
2024-06-01 22:37:39 +0800

    add page contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 //page
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Project } from "./pages/Project";
+import { Contact } from "./pages/Contact";
 import ErrorPage from "./pages/Error";
 
 //components
 import { Header } from "./components/Layouts/Header";
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <div className="pages">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Header />
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,139 @@
+import { useRef, useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export const Contact = () => {
+  const [yourName, setYourName] = useState("");
+  const [yourEmail, setYourEmail] = useState("");
+  const [yourMessage, setYourMessage] = useState("");
+  const [fields, setFields] = useState([]);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  const inputRef = useRef("");
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      service_id: import.meta.env.REACT_APP_SERVICE_ID,
+      template_id: import.meta.env.REACT_APP_TEMPLATE_ID,
+      user_id: import.meta.env.REACT_APP_USER_ID,
+      template_params: {
+        from_name: yourName,
+        from_email: yourEmail,
+        to_name: "Iqoqacola",
+        message: yourMessage,
+      },
+    };
+
+    let emptyFields = [];
+
+    if (yourName === "") {
+      emptyFields.push("yourName");
+    }
+
+    if (!yourEmail) {
+      emptyFields.push("yourEmail");
+    }
+
+    if (!yourMessage) {
+      emptyFields.push("yourMessage");
+    }
+
+    if (emptyFields.length > 0) {
+      setFields(emptyFields);
+      setError("Please fill in all the fields");
+      setSuccess("");
+      return;
+    }
+
+    try {
+      const response = await fetch(import.meta.env.REACT_APP_CONTACT_URI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setSuccess(
+          "Thank you for reaching out me. I will get back to you soon!"
+        );
+        setError("");
+        setYourName("");
+        setYourEmail("");
+        setYourMessage("");
+        setFields([]);
+      }
+    } catch (error) {
+      setSuccess("");
+      setFields([]);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+    AOS.init({
+      duration: 1000,
+    });
+    inputRef.current.focus();
+  }, []);
+  return (
+    <>
+      <section className="pt-28 w-full">
+        <div
+          className="container w-2/3 lg:w-1/3 contact-form"
+          data-aos="fade-up"
+        >
+          <form onSubmit={sendEmail} className="contact-me flex flex-col">
+            <h2 className="text-center text-2xl font-bold text-red-400 drop-shadow-md">
+              Contact me!
+            </h2>
+            <label>Your Name</label>
+            <input
+              type="text"
+              name="user-name"
+              onChange={(e) => {
+                setYourName(e.target.value);
+              }}
+              value={yourName}
+              className={`${fields.includes("yourName") ? "error" : ""}`}
+              placeholder="Full Name"
+              ref={inputRef}
+            />
+            <label>Your Email</label>
+            <input
+              type="email"
+              name="user-email"
+              onChange={(e) => {
+                setYourEmail(e.target.value);
+              }}
+              value={yourEmail}
+              className={`${fields.includes("yourEmail") ? "error" : ""}`}
+              placeholder="example@email.com"
+            />
+            <label>Message</label>
+            <textarea
+              type="text"
+              name="user-message"
+              onChange={(e) => {
+                setYourMessage(e.target.value);
+              }}
+              value={yourMessage}
+              className={`${fields.includes("yourMessage") ? "error" : ""}`}
+              placeholder="Enter Your Message"
+            />
+            <button className="sendEmail">Send</button>
+            {success && <div className="success">{success}</div>}
+            {error && <div className="error">{error}</div>}
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
